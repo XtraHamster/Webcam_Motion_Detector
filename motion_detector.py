fe4772b33b1e55d1e70e9a92c74bd81ac3def291
XtraHamster <martinkrecek98@seznam.cz>
2020-07-28 16:14:46 +0200

    first_frame and GausssianBlur
    Added first_frame variable which will be used as comparasion to other images and difference between pictures can be detect as move. Added Gaussian blur for bettter detection conditions.

--- motion_detector.py
+++ motion_detector.py
@@ -1,10 +1,20 @@
 import cv2, time
 
+first_frame = None  # Define a first frame for next steps
+
 video = cv2.VideoCapture(0)  # Capturing a video
 
 while True:
     check, frame = video.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Converting a video to gray colors
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)  # Adding Gaussian blur --> better noise cancel, better conditions for motion detecting
+
+    if first_frame is None:  # Assign first_frame to a variable, if already does not have
+        first_frame = gray
+        continue
+    
+        
+
     cv2.imshow("Capturing", gray)
     key = cv2.waitKey(1)  # Adding some waiting time between frames (adjusting FPS)
     print(gray) 
