8be063b9ac5dc4a3990e3a1a107577eaf19fc48a
XtraHamster <martinkrecek98@seznam.cz>
2020-07-28 16:49:18 +0200

    Contours
    Added contours to with new Color Frame. Moving objects are specified by green rectangles.

--- motion_detector.py
+++ motion_detector.py
@@ -14,12 +14,21 @@ while True:
         continue
     
     delta_frame = cv2.absdiff(first_frame, gray)
+    thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]  # Added threshold 
+    thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
-    thresh_delta = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    (cnts,_) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in cnts:
+        if cv2.contourArea(contour) < 1000:
+            continue
+        (x, y, w, h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x + w, h + y), (0, 255, 0), 3)
 
     cv2.imshow("Grey Frame", gray)
     cv2.imshow("Delta Frame", delta_frame)
-    cv2.imshow("Threshold Frame", thresh_delta)
+    cv2.imshow("Threshold Frame", thresh_frame)
+    cv2.imshow("Color Frame", frame)
 
     key = cv2.waitKey(1)  # Adding some waiting time between frames (adjusting FPS)
     print(gray) 
